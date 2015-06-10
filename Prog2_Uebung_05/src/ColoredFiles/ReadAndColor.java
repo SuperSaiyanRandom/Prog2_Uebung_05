38a20d4e9a44eb512717040b1492730eb1ab4f79
Super Saiyan <SuperSaiyanRandom@gmail.com>
2015-06-10 17:02:57 +0200

    Method implementation compleated: ReadAndColor.readFile()

--- Prog2_Uebung_05/src/ColoredFiles/ReadAndColor.java
+++ Prog2_Uebung_05/src/ColoredFiles/ReadAndColor.java
@@ -12,6 +12,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -32,7 +33,12 @@ public class ReadAndColor {
 		JLabel label = new JLabel("Color");
 		frame.add(label);
 		
-		//TODO 
+		/* 
+		 * choose random color from c[]
+		 * make rColor the labels color for text
+		*/
+		Color rColor = c[ (int) (Math.random()*c.length) ];
+		label.setForeground(rColor);
 		
 		frame.setVisible(true);
 		frame.setSize(200, 200);
@@ -42,8 +48,65 @@ public class ReadAndColor {
 	}
 
 	private Color[] readFile() throws IOException {
-		//TODO
-		return null;
+		
+		Color[] listColor = null;
+		
+		// create FileChooser Object
+		File currentDirectory = new File(System.getProperty("user.dir"));
+		choser = new JFileChooser(currentDirectory);
+		
+		/*
+		 * FileChooser is opened
+		 * returnVal saved the state of FileChooser on popdown
+		 * 	•JFileChooser.CANCEL_OPTION 
+		 * 	•JFileChooser.APPROVE_OPTION 
+		 * 	•JFileChooser.ERROR_OPTION
+		 */
+		int returnVal = choser.showOpenDialog(null);
+		
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			File file =  choser.getSelectedFile();
+			
+			Color tempColor;
+			int red, green, blue;
+			String currentLine;
+			String[] rgbStringList;
+			
+			/*
+			 * read the file chosen from JFileChooser line by line
+			 * set the number of colors in listColor to the number of lines in file
+			 * create a color-object out of the information in each line
+			 */
+			
+			read = new BufferedReader(new FileReader(file));
+			
+			/* getSizeOfFile(BufferedReader) doesn't use the read BufferedReader but
+			 * creates a equal new one, because the method getSizeOfFile closes the
+			 * BufferedReader after using it. The BufferedReader is however still needed...
+			 */
+			listColor = new Color[getSizeOfFile(new BufferedReader(new FileReader(file)))];
+			
+			int count = 0;
+			while((currentLine = read.readLine()) != null) {
+				
+				rgbStringList = currentLine.split(",");
+				
+				// TODO: if String Array is not 3 parts (red,green,blue) skip this string
+				// Better: use String Format
+				if(!(rgbStringList.length == 3))
+					continue;
+				
+				red 	= Integer.parseInt(rgbStringList[0]);
+				green 	= Integer.parseInt(rgbStringList[1]);
+				blue 	= Integer.parseInt(rgbStringList[2]);
+				tempColor = new Color(red,green,blue);
+				
+				listColor[count] = tempColor;
+				count++;
+			}
+		}
+		
+		return listColor;
 	}
 
 	private int getSizeOfFile(BufferedReader read) throws IOException {
